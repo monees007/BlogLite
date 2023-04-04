3e38fdaefe4abd049cc7a1470fc7e787e3b9184b
monees007 <monees007@outlook.com>
2023-04-04 22:58:10 +0530

    temp-secret-removed

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID",
-                                  '846448875684-5kca9r1coj09ha193jhist3j66m7mcvs.apps.googleusercontent.com')
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "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")
+                                  'temp-secret-removed')
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "temp-secret-removed")
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
